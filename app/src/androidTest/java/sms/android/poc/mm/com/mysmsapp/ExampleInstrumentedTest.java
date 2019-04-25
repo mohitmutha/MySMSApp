1ba582eb8ccf92e29eba4f5cd1e3baa287d249fc
Mohit <mohitmutha@gmail.com>
2019-04-25 22:39:01 +0530

    Initial commit

---
+++ app/src/androidTest/java/sms/android/poc/mm/com/mysmsapp/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package sms.android.poc.mm.com.mysmsapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("sms.android.poc.mm.com.mysmsapp", appContext.getPackageName());
+    }
+}
